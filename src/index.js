0fba0e1da7245904e2918f21fc0f856b4f0c152b
ralphZero <placideralph@gmail.com>
2021-02-25 00:42:45 -0500

    init firebase

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { RootReducer } from './store/reducers/RootReducer';
+import { createFirestoreInstance  } from 'redux-firestore';
+import firebase from './config/firebase';
+import thunk from 'redux-thunk';
+
+const store = createStore(RootReducer, applyMiddleware(thunk.withExtraArgument({ getFirebase })));
+
+/* const rrfConfig = {
+  userProfile: 'users', 
+  useFirestoreForProfile: true
+} */
+
+const rrfProps = {
+  firebase,
+  config: {},
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/store/reducers/RootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers  } from 'redux';
+import { firebaseReducer } from 'react-redux-firebase';
+import { firestoreReducer } from 'redux-firestore';
+
+export const RootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+});
